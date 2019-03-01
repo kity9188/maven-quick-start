3f73d2142bda89db51ba3887303e6545ef5ee634
nicks9188 <nicks9188@gmail.com>
2019-03-01 19:02:06 +0530

    Adding the .jar file post to 'mvn package'

---
+++ src/main/java/Chaudhary/Chacha/Application.java
@@ -0,0 +1,16 @@
+package Chaudhary.Chacha;
+
+public class Application {
+
+	// method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+		System.out.println ("Starting Application");
+		Application app = new Application();
+    }
+	
+	public Application() {
+	
+		System.out.println ("Inside Application");
+    }
+
+}
