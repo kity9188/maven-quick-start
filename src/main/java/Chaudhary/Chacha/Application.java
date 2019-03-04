c616b53ce13a072cf8dfa5e9d575c24a2b9f9844
nicks9188 <nicks9188@gmail.com>
2019-03-05 00:02:03 +0530

    adding updated pom and source code

--- src/main/java/Chaudhary/Chacha/Application.java
+++ src/main/java/Chaudhary/Chacha/Application.java
@@ -1,16 +1,16 @@
 package Chaudhary.Chacha;
 
-/**import java.util.ArrayList;
-import java.util.list;
-import org.apache.commons.lang3.StringUtils;
-**/
+import java.util.ArrayList;     //a list is simply an interface and the array list is the implementation of that interface.
+import java.util.List;
+//import org.apache.commons.lang3.StringUtils;
+
 public class Application {
 
 	// method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 		System.out.println ("Starting Application");
 		Application app = new Application();
-		//app.greet();
+		app.greet();
 		//int count = app.countWords("I've four words");
 		//System.out.println("Word Count: "+count);
     }
@@ -20,15 +20,15 @@ public class Application {
 		System.out.println ("Inside Application");
     }
 
-/**	public void greet(){
-		List<String> greetings = new ArrayList<> ();
+    public void greet(){
+		List<String> greetings = new ArrayList<> ();   // list and array list within method called greet.
 		greetings.add("Hello");
 		
 		for (String greeting : greetings){
 			System.out.println("Greeting: "+ greeting);
 		}
 	}
-	public int countWords (String words){
+/**	public int countWords (String words){
 		String[] seperateWords = StringUtils.split(words, ' ');
 		return (seperateWords == null) ? 0 : seperateWords.length;
 	}   **/
